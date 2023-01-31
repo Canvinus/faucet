866c6b2a59c822f0d148f881fcba943f6bb1aee4
Andrey Gruzdev <agruzdev1@gmail.com>
2023-01-31 23:44:14 +0100

    Init commit

---
+++ contracts/Faucet.sol
@@ -0,0 +1,39 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.22 <0.9.0;
+
+contract Faucet {
+
+    uint public numOfFunders;
+    mapping(uint => address) public funders;
+
+    receive() external payable {}
+
+    function withdraw(uint256 ammount) external {
+        uint wei_ammount = eth_to_wei(ammount);
+        if (wei_ammount < eth_to_wei(10)){
+            payable(msg.sender).transfer(wei_ammount);
+        }
+    }
+
+    function addFunds() external payable {
+        funders[numOfFunders++] = msg.sender;
+    }
+
+    function getFundersAt(uint8 index) external view returns(address) {
+        return funders[index];
+    }
+
+    function getAllFunders() external view returns(address[] memory) {
+        address[] memory _funders = new address[](numOfFunders);
+
+        for (uint i = 0; i < numOfFunders; i++) {
+            _funders[i] = funders[i];
+        }
+
+        return _funders;
+    }
+
+    function eth_to_wei(uint256 eth) internal pure returns(uint256) {
+        return eth * 1000000000000000000;
+    }
+}
--- src/App.js
+++ src/App.js
@@ -1,24 +1,88 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
+import Web3 from 'web3';
+import detectEthereumProvider from '@metamask/detect-provider';
+import { loadContract } from './utils/loadContract';
 import './App.css';
 
 function App() {
+  const [web3Api, setWeb3Api] = useState({
+    provider: null,
+    web3: null,
+    contract: null
+  });
+
+  const [account, setAccount] = useState(null);
+
+  const [balance, setBalance] = useState(null);
+
+  useEffect(() => {
+    const loadProvider = async () => {
+      const provider = await detectEthereumProvider();
+      const contract = await loadContract("Faucet", provider);
+
+      if (!provider) {
+        console.log('Install metamask!');
+        return;
+      }
+
+      provider.request({method: 'eth_requestAccounts'});
+      setWeb3Api({
+        provider,
+        web3: new Web3(provider),
+        contract
+      });
+    }
+
+    loadProvider();
+  }, []);
+
+  useEffect(() => {
+    const loadBalance = async () => {
+      const { contract, web3 } = web3Api;
+      const balance = await web3.eth.getBalance(contract.address);
+      setBalance(web3.utils.fromWei(balance, 'ether'));
+    }
+
+    web3Api.contract && loadBalance();
+  }, [web3Api]);
+
+  useEffect(() => {
+    const getAccount = async () => {
+      const accounts = await web3Api.web3.eth.getAccounts();
+      setAccount(accounts[0]);
+    }
+
+    web3Api.web3 && getAccount();
+  }, [web3Api.web3]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className='faucet-wrapper'>
+        <div className='faucet'>
+          <div className='account-wrapper'>
+            <span>
+              <strong className='mr-2 mb-2'>Account: </strong>
+            </span>
+            {account ? 
+              account : 
+              <button 
+              className='button is-small' 
+              onClick={() => {web3Api.provider.request({method: 'eth_requestAccounts'})}}
+              >
+                Connect Wallet
+              </button>
+            }
+          </div>
+          <div className='balance-view is-size-2 mr-2 mb-2'>
+            Current Balance: <strong className='mr-2'>{balance}</strong>ETH
+          </div>
+          <div className='buttons-wrapper'>
+            <button className='button is-success is-light mr-2'>Donate</button>
+            <button className='button is-danger is-light'>Withdraw</button>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
